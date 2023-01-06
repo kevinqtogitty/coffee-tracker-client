9aee654a9b0f7020d6359225e4f7be970f276377
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-06 22:04:57 +0100

    Initial commit

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import { User } from 'firebase/auth';
+import React from 'react';
+import Form from '../components/Form';
+
+const LoginPage: React.FC = () => {
+  return (
+    <main>
+      <Form />
+    </main>
+  );
+};
+
+export default LoginPage;
