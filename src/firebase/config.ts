7d5fb2887dae0996d6e0b5d5e032b4b8744cb816
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-06 22:05:31 +0100

    Update: firebase auth initilization moved into this file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,20 +1,18 @@
-import { initializeApp } from 'firebase/app';
-
-import dotenv from 'dotenv';
-dotenv.config();
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { FirebaseApp, initializeApp } from 'firebase/app';
+import { Auth, getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOM,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
-  appId: process.env.FB_APP_ID
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOM,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app: FirebaseApp = initializeApp(firebaseConfig);
+
 // Initialize Firebase Auth
+export const auth: Auth = getAuth(app);
