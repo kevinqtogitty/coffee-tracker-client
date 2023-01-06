cbb1fdb4a43b6ff71b79c3e5fd7c68d2ad474b7b
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-06 22:07:04 +0100

    Update: added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { useState } from 'react';
+import { Auth, getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useNavigate
+} from 'react-router-dom';
+import Form from './components/Form';
+import { app } from './firebase/config';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import { AuthRoutes } from './utils/AuthRoute';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>hello</>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<AuthRoutes />}>
+          <Route element={<HomePage />} path="/home" />
+        </Route>
+        <Route element={<LoginPage />} path="/" />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
