dc34828cef08dfc17edd0e0b8f773ae9d2c8afd7
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-07 18:37:42 +0100

    Update: added axios requests to fetch form data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
-import Form from '../components/Form';
+import React, { useEffect, useState } from 'react';
+import Navigation from '../components/Navigation';
+import { auth } from '../firebase/config';
+// import (getAllUsersCoffees) from '../requests/coffeeRequests.ts'
+import { getAllUsersCoffees } from '../requests/coffeeRequests';
 
-const HomePage = () => {
-  return <main>HomePage</main>;
+interface HomePageProps {
+  usersCoffees: [];
+  setUsersCoffees: React.Dispatch<React.SetStateAction<[]>>;
+  setCurrentUser: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const HomePage: React.FC<HomePageProps> = ({
+  usersCoffees,
+  setUsersCoffees,
+  setCurrentUser
+}) => {
+  const fetchUsersCoffeeData = async (userId: string) => {
+    const data = await getAllUsersCoffees(userId);
+    setUsersCoffees(data);
+  };
+
+  if (auth.currentUser && usersCoffees.length === 0) {
+    console.log('fetching users coffeers');
+    fetchUsersCoffeeData(auth.currentUser.uid);
+  }
+
+  return (
+    <section className="home-page">
+      <Navigation setCurrentUser={setCurrentUser} />
+    </section>
+  );
 };
 
 export default HomePage;
