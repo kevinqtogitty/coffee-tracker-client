c390f8e1e19c0f31d166f0ff2a83562c89909e12
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-09 19:13:15 +0100

    Update: moved data fetching to homepage

--- src/pages/AddNewCoffeePage.tsx
+++ src/pages/AddNewCoffeePage.tsx
@@ -5,31 +5,12 @@ import { getAllOrigins } from '../requests/originRequests';
 import { getAllProcesses } from '../requests/processesrequests';
 
 interface AddNewCoffeePageProps {
-  processes: [];
-  setProcesses: React.Dispatch<React.SetStateAction<[]>>;
-  origins: [];
-  setOrigins: React.Dispatch<React.SetStateAction<[]>>;
   setCurrentUser: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const AddNewCoffeePage: React.FC<AddNewCoffeePageProps> = ({
-  processes,
-  origins,
-  setProcesses,
-  setOrigins,
   setCurrentUser
 }) => {
-  const fetchFormFields = async () => {
-    const origins = await getAllOrigins();
-    const processes = await getAllProcesses();
-    setProcesses(processes);
-    setOrigins(origins);
-  };
-
-  if (auth.currentUser && processes.length === 0) {
-    console.log('fetching form fields');
-    fetchFormFields();
-  }
   return (
     <section className="add-coffee-page">
       <Navigation setCurrentUser={setCurrentUser} />
