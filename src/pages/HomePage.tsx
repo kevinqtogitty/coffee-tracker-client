c2e1c92cfaebce214f051fd7649a192819b5d93b
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-09 19:12:25 +0100

    Update: added new props to be passed down from App level

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,22 +4,46 @@ import Navigation from '../components/Navigation';
 import { auth } from '../firebase/config';
 // import (getAllUsersCoffees) from '../requests/coffeeRequests.ts'
 import { getAllUsersCoffees } from '../requests/coffeeRequests';
-import { UsersCoffee } from '../types/types';
+import { getAllOrigins } from '../requests/originRequests';
+import { getAllProcesses } from '../requests/processesrequests';
+import { getAllRoastLevels } from '../requests/roastLevelRequests';
+import { Origins, Processes, RoastLevels, UsersCoffee } from '../types/types';
 
 interface HomePageProps {
   usersCoffees: UsersCoffee[];
+  processes: Processes[];
+  origins: Origins[];
+  setProcesses: React.Dispatch<React.SetStateAction<Processes[]>>;
+  setOrigins: React.Dispatch<React.SetStateAction<Origins[]>>;
   setUsersCoffees: React.Dispatch<React.SetStateAction<UsersCoffee[]>>;
   setCurrentUser: React.Dispatch<React.SetStateAction<boolean>>;
+  setRoastLevels: React.Dispatch<React.SetStateAction<RoastLevels[]>>;
+  roastLevels: RoastLevels[];
 }
 
 const HomePage: React.FC<HomePageProps> = ({
   usersCoffees,
   setUsersCoffees,
-  setCurrentUser
+  setCurrentUser,
+  setProcesses,
+  setOrigins,
+  processes,
+  origins,
+  roastLevels,
+  setRoastLevels
 }) => {
   const fetchUsersCoffeeData = async (userId: string) => {
     const data = await getAllUsersCoffees(userId);
     setUsersCoffees(data);
+  };
+
+  const fetchFormFields = async () => {
+    const origins = await getAllOrigins();
+    const processes = await getAllProcesses();
+    const roastLevels = await getAllRoastLevels();
+    setOrigins(origins);
+    setProcesses(processes);
+    setRoastLevels(roastLevels);
   };
 
   if (auth.currentUser && usersCoffees.length === 0) {
@@ -27,10 +51,21 @@ const HomePage: React.FC<HomePageProps> = ({
     fetchUsersCoffeeData(auth.currentUser.uid);
   }
 
+  if (auth.currentUser && processes.length === 0) {
+    console.log('fetching form fields');
+    fetchFormFields();
+  }
+
   return (
     <section className="home-page">
       <Navigation setCurrentUser={setCurrentUser} />
-      <CoffeeAccordion usersCoffees={usersCoffees} />
+      <CoffeeAccordion
+        usersCoffees={usersCoffees}
+        origins={origins}
+        processes={processes}
+        roastLevels={roastLevels}
+        fetchUsersCoffeeData={fetchUsersCoffeeData}
+      />
     </section>
   );
 };
