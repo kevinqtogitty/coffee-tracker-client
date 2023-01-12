8a3b3a369529dde4b942053d1ce4fdb901acfce3
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-12 12:12:37 +0100

    Update: jsx props moved into an object and spreaded for drilling

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,14 @@ import AddNewCoffeePage from './pages/AddNewCoffeePage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import { getAllUsersCoffees } from './requests/coffeeRequests';
-import { Origins, Processes, RoastLevels, UsersCoffee } from './types/types';
+import { getCurrentUserData } from './requests/userRequests';
+import {
+  Origins,
+  Processes,
+  RoastLevels,
+  User,
+  UsersCoffee
+} from './types/types';
 
 import { AuthRoutes } from './utils/AuthRoute';
 
@@ -20,7 +27,9 @@ function App() {
   const [processes, setProcesses] = useState<Processes[]>([]);
   const [roastLevels, setRoastLevels] = useState<RoastLevels[]>([]);
   const [usersCoffees, setUsersCoffees] = useState<UsersCoffee[]>([]);
-  const [currentUser, setCurrentUser] = useState<boolean>(false);
+  const [currentUserLoggedIn, setCurrentUserLoggedIn] =
+    useState<boolean>(false);
+  const [currentUserInfo, setCurrentUserInfo] = useState<User>();
 
   /*
     We can't call useNavigate outside a react-router-dom route so 
@@ -31,7 +40,7 @@ function App() {
   auth.onAuthStateChanged((user) => {
     if (user) {
       console.log('User has logged in');
-      setCurrentUser(true);
+      setCurrentUserLoggedIn(true);
     } else {
       console.log('user is not logged in');
     }
@@ -42,45 +51,58 @@ function App() {
     setUsersCoffees(data);
   };
 
+  const fetchCurrentUserInfo = async (userId: string) => {
+    const data = await getCurrentUserData(userId);
+    setCurrentUserInfo(data);
+  };
+
+  const homepageProps = {
+    currentUserInfo: currentUserInfo,
+    usersCoffees: usersCoffees,
+    processes: processes,
+    origins: origins,
+    roastLevels: roastLevels,
+    setRoastLevels: setRoastLevels,
+    setUsersCoffees: setUsersCoffees,
+    setCurrentUserLoggedIn: setCurrentUserLoggedIn,
+    setOrigins: setOrigins,
+    setProcesses: setProcesses,
+    fetchUsersCoffeeData: fetchUsersCoffeeData,
+    setCurrentUserInfo: setCurrentUserInfo,
+    fetchCurrentUserInfo: fetchCurrentUserInfo
+  };
+
+  const addNewCoffeeProps = {
+    origins: origins,
+    processes: processes,
+    roastLevels: roastLevels,
+    fetchUsersCoffeeData: fetchUsersCoffeeData,
+    setCurrentUserLoggedIn: setCurrentUserLoggedIn
+  };
+
   return (
     <Router>
       <Routes>
         <Route element={<AuthRoutes />}>
           <Route
-            element={<AccountSettings setCurrentUsers={setCurrentUser} />}
+            element={
+              <AccountSettings
+                setCurrentUserLoggedIn={setCurrentUserLoggedIn}
+                currentUserInfo={currentUserInfo}
+              />
+            }
             path="/account"
-          ></Route>
-          <Route
-            element={
-              <HomePage
-                usersCoffees={usersCoffees}
-                processes={processes}
-                origins={origins}
-                roastLevels={roastLevels}
-                setRoastLevels={setRoastLevels}
-                setUsersCoffees={setUsersCoffees}
-                setCurrentUser={setCurrentUser}
-                setOrigins={setOrigins}
-                setProcesses={setProcesses}
-                fetchUsersCoffeeData={fetchUsersCoffeeData}
-              />
-            }
-            path="/home"
           />
+          <Route element={<HomePage {...homepageProps} />} path="/home" />
           <Route
-            element={
-              <AddNewCoffeePage
-                origins={origins}
-                processes={processes}
-                roastLevels={roastLevels}
-                fetchUsersCoffeeData={fetchUsersCoffeeData}
-                setCurrentUser={setCurrentUser}
-              />
-            }
+            element={<AddNewCoffeePage {...addNewCoffeeProps} />}
             path="/addNewCoffee"
           />
         </Route>
-        <Route element={<LoginPage currentUser={currentUser} />} path="/" />
+        <Route
+          element={<LoginPage currentUserLoggedIn={currentUserLoggedIn} />}
+          path="/"
+        />
       </Routes>
     </Router>
   );
