c95c0ee5b954c4c7628b32f91d69ed32a56e9163
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-13 22:04:47 +0100

    -

--- src/pages/AccountSettings.tsx
+++ src/pages/AccountSettings.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import AccountDetailsForm from '../components/forms/AccountDetailsForm';
+import Dialog from '../components/Dialog';
 
 interface AccountSettingProps {
   setCurrentUserLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
@@ -13,14 +15,21 @@ const AccountSettings: React.FC<AccountSettingProps> = ({
   return (
     <section className="account-settings-page">
       <Navigation setCurrentUserLoggedIn={setCurrentUserLoggedIn} />
-      <h2>Your account details:</h2>
-      <article>
-        <p>First name: {currentUserInfo?.first_name}</p>
-        <p>Last name: {currentUserInfo?.last_name}</p>
-        <p>Email: {currentUserInfo?.email}</p>
-        <p>
-          Reset password: <button>send email reset</button>
-        </p>
+      <article className="account-info-container">
+        <div>
+          <h2>Your account details:</h2>
+          <p>First name: {currentUserInfo?.first_name}</p>
+          <p>Last name: {currentUserInfo?.last_name}</p>
+          <p>Email: {currentUserInfo?.email}</p>
+          <p>
+            Reset password: <button>send email reset</button>
+          </p>
+        </div>
+        <Dialog
+          AccountDetailsForm={AccountDetailsForm}
+          currentUserInfo={currentUserInfo}
+          formKey={0}
+        />
       </article>
     </section>
   );
