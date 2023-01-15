80edc644286836eb9058f5ee7d8dbc7c0bd0e1bb
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-15 19:48:19 +0100

    Update: added fetchUsersIngo to page props

--- src/pages/AccountSettings.tsx
+++ src/pages/AccountSettings.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import AccountDetailsForm from '../components/forms/AccountDetailsForm';
 import Dialog from '../components/Dialog';
+import { updateUser } from '../requests/userRequests';
+import { auth } from '../firebase/config';
 
 interface AccountSettingProps {
   setCurrentUserLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
   currentUserInfo?: any;
+  fetchCurrentUserInfo: (userId: string) => Promise<void>;
 }
 
 const AccountSettings: React.FC<AccountSettingProps> = ({
   setCurrentUserLoggedIn,
-  currentUserInfo
+  currentUserInfo,
+  fetchCurrentUserInfo
 }) => {
+  const resetPassword = () => {
+    updateUser(auth.currentUser?.uid!, 'forgotPassword', currentUserInfo);
+  };
+
   return (
     <section className="account-settings-page">
       <Navigation setCurrentUserLoggedIn={setCurrentUserLoggedIn} />
@@ -22,13 +30,17 @@ const AccountSettings: React.FC<AccountSettingProps> = ({
           <p>Last name: {currentUserInfo?.last_name}</p>
           <p>Email: {currentUserInfo?.email}</p>
           <p>
-            Reset password: <button>send email reset</button>
+            Reset password:{' '}
+            <button onClick={resetPassword} className="password-reset">
+              send email reset
+            </button>
           </p>
         </div>
         <Dialog
           AccountDetailsForm={AccountDetailsForm}
           currentUserInfo={currentUserInfo}
           formKey={0}
+          fetchCurrentUserInfo={fetchCurrentUserInfo}
         />
       </article>
     </section>
