aba50055c2308efbc0ea6e07336f9aebd3aba118
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-15 21:55:27 +0100

    Update: added email validation cuntion

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -28,4 +28,10 @@ const findOptionId = (
   return optionFound.id;
 };
 
-export { convertToBoolean, findOptionId };
+const isValidEmail = (email: string) =>
+  // eslint-disable-next-line no-useless-escape
+  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+
+export { convertToBoolean, findOptionId, isValidEmail };
