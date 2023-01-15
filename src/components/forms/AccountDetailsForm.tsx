bce5c8b58ba27cd9215c3e79197cd1972460513b
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-01-15 19:49:30 +0100

    Update: added update user data function

--- src/components/forms/AccountDetailsForm.tsx
+++ src/components/forms/AccountDetailsForm.tsx
@@ -1,13 +1,19 @@
+import { Close } from '@radix-ui/react-dialog';
+import { ColumnSpacingIcon } from '@radix-ui/react-icons';
 import { setLogLevel } from 'firebase/app';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { auth } from '../../firebase/config';
+import { updateUser } from '../../requests/userRequests';
 
 export interface AccountDetailsFormProps {
   currentUserInfo: any;
+  fetchCurrentUserInfo: (userId: string) => Promise<void>;
 }
 
 const AccountDetailsForm: React.FC<AccountDetailsFormProps> = ({
-  currentUserInfo
+  currentUserInfo,
+  fetchCurrentUserInfo
 }) => {
   const {
     register,
@@ -21,27 +27,51 @@ const AccountDetailsForm: React.FC<AccountDetailsFormProps> = ({
       email: currentUserInfo.email
     }
   });
-  const submitData = (data: any) => {
-    console.log(data);
+  const submitData = async (data: any) => {
+    try {
+      if (auth.currentUser?.email != data.email) {
+        await updateUser(auth.currentUser?.uid!, 'email', data);
+        fetchCurrentUserInfo(auth.currentUser?.uid!);
+      } else if (
+        data.firstName != currentUserInfo.first_name ||
+        data.lastName != currentUserInfo.last_name
+      ) {
+        updateUser(auth.currentUser?.uid!, 'name', data);
+        fetchCurrentUserInfo(auth.currentUser?.uid!);
+      } else {
+        console.log('No changes made');
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  // console.log(fetchCurrentUserInfo);
+
   return (
     <form
       className="account-details-form"
       onSubmit={handleSubmit((data) => submitData(data))}
     >
-      <div className="form-input-label-container">
-        <label htmlFor="">First name:</label>
-        <input type="text" {...register('firstName')} />
+      <div className="account-form-input-container">
+        <div className="form-input-label-container">
+          <label htmlFor="">First name:</label>
+          <input type="text" {...register('firstName')} />
+        </div>
+        <div className="form-input-label-container">
+          <label htmlFor="">Last name:</label>
+          <input type="text" {...register('lastName')} />
+        </div>
+        <div className="form-input-label-container">
+          <label htmlFor="">Email:</label>
+          <input type="email" {...register('email')} />
+        </div>
       </div>
-      <div className="form-input-label-container">
-        <label htmlFor="">Last name:</label>
-        <input type="text" {...register('lastName')} />
-      </div>
-      <div className="form-input-label-container">
-        <label htmlFor="">Email:</label>
-        <input type="email" {...register('email')} />
-      </div>
-      <button type="submit">Save</button>
+      {/* <Close asChild> */}
+      <button className="form-save-button" type="submit">
+        Save
+      </button>
+      {/* </Close> */}
     </form>
   );
 };
