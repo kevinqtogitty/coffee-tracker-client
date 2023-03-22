aef365be9200e69201fc9dcac8ee3def2f400179
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-03-22 13:49:28 -0400

    Update: added timestamp property to reset values

--- src/components/forms/AddNewCoffeeForm.tsx
+++ src/components/forms/AddNewCoffeeForm.tsx
@@ -23,6 +23,7 @@ const AddNewCoffeeForm: React.FC<AddNewCoffeePageProps> = ({
     singleOrigin: false,
     country: '',
     notes: '',
+    timestamp: '',
     farmer: null
   };
 
@@ -86,139 +87,152 @@ const AddNewCoffeeForm: React.FC<AddNewCoffeePageProps> = ({
       className="edit-coffee-form add-coffee"
       onSubmit={handleSubmit((data) => logData(data))}
     >
-      <div className="form-field-container add-coffee">
-        <div className="form-input-label-container">
-          <Label className="edit-coffee-label" htmlFor="coffeeName">
-            Name
-          </Label>
-          <input
-            className="edit-coffee-input"
-            type="text"
-            {...register('name', {
-              required: 'This is required',
-              maxLength: {
-                value: 50,
-                message: 'Max length is 50 chars'
-              }
-            })}
-          />
-          <span className="form-error-span">{errors.name?.message}</span>
-        </div>
-        <div className="form-input-label-container">
-          <Label className="edit-coffee-label" htmlFor="roaster">
-            Roaster
-          </Label>
-          <input
-            className="edit-coffee-input"
-            type="text"
-            {...register('roaster', {
-              required: 'This is required',
-              maxLength: {
-                value: 50,
-                message: 'Max length is 50 chars'
-              }
-            })}
-          />
-          <span className="form-error-span">{errors.roaster?.message}</span>
-        </div>
-        <div className="form-input-label-container">
-          <Label className="edit-coffee-label" htmlFor="price">
-            Price
-          </Label>
-          <input
-            className="edit-coffee-input"
-            type="text"
-            {...register('price', {
-              required: 'This is required',
-              maxLength: { value: 5, message: 'Maximum chars is 5' }
-            })}
-          />
-          <span className="form-error-span">{errors.price?.message}</span>
-        </div>
-        <div className="form-input-label-container">
-          <fieldset className="edit-coffee-fieldset">
-            <legend className="edit-coffee-legend">Single Origin</legend>
-            <div className="edit-coffee-radio-wrapper">
-              <input
-                {...register('singleOrigin')}
-                type="radio"
-                value="true"
-                id="true"
-              />
-              <Label className="edit-coffee-label" htmlFor="true">
-                True
+      <h2 className="form-h2">Add Coffee Details Below</h2>
+      <div>
+        <section className="add-coffee-form-section one">
+          <div className="form-field-container add-coffee">
+            <div className="form-input-label-container">
+              <Label className="add-coffee-label" htmlFor="coffeeName">
+                Name
               </Label>
+              <input
+                className="add-coffee-input"
+                type="text"
+                {...register('name', {
+                  required: 'This is required',
+                  maxLength: {
+                    value: 50,
+                    message: 'Max length is 50 chars'
+                  }
+                })}
+              />
+              <span className="form-error-span">{errors.name?.message}</span>
+            </div>
+            <div className="form-input-label-container">
+              <Label className="add-coffee-label" htmlFor="roaster">
+                Roaster
+              </Label>
+              <input
+                className="add-coffee-input"
+                type="text"
+                {...register('roaster', {
+                  required: 'This is required',
+                  maxLength: {
+                    value: 50,
+                    message: 'Max length is 50 chars'
+                  }
+                })}
+              />
+              <span className="form-error-span">{errors.roaster?.message}</span>
+            </div>
+            <div className="form-input-label-container">
+              <Label className="add-coffee-label" htmlFor="price">
+                Price
+              </Label>
+              <input
+                className="add-coffee-input"
+                type="text"
+                {...register('price', {
+                  required: 'This is required',
+                  maxLength: { value: 5, message: 'Maximum chars is 5' }
+                })}
+              />
+              <span className="form-error-span">{errors.price?.message}</span>
+            </div>
+            <div className="form-input-label-container">
+              <fieldset className="edit-coffee-fieldset">
+                <legend className="edit-coffee-legend">Single Origin</legend>
+                <div className="edit-coffee-radio-wrapper">
+                  <Label className="add-coffee-label radio" htmlFor="true">
+                    <input
+                      {...register('singleOrigin')}
+                      type="radio"
+                      value="true"
+                      id="true"
+                      className="radio-button"
+                    />
+                    True
+                  </Label>
+                  <span className="form-error-span">
+                    {errors.singleOrigin?.message}
+                  </span>
+                </div>
+                <div className="edit-coffee-radio-wrapper">
+                  <Label className="add-coffee-label radio" htmlFor="false">
+                    <input
+                      {...register('singleOrigin')}
+                      type="radio"
+                      value="false"
+                      id="false"
+                      className="radio-button"
+                    />
+                    False
+                  </Label>
+                  <span className="form-error-span">
+                    {errors.singleOrigin?.message}
+                  </span>
+                </div>
+              </fieldset>
+            </div>
+          </div>
+          <div className="select-container">
+            <div className="form-input-label-container">
+              <Label className="add-coffee-label" htmlFor="roastLevels">
+                Roast level
+              </Label>
+              <SelectField
+                option={'roastLevels'}
+                options={roastLevels}
+                register={register}
+              />
               <span className="form-error-span">
-                {errors.singleOrigin?.message}
+                {errors.roastLevel?.message}
               </span>
             </div>
-            <div className="edit-coffee-radio-wrapper">
-              <input
-                {...register('singleOrigin')}
-                type="radio"
-                value="false"
-                id="false"
-              />
-              <Label className="edit-coffee-label" htmlFor="false">
-                False
+
+            <div className="form-input-label-container">
+              <Label className="add-coffee-label" htmlFor="process">
+                Process
               </Label>
-              <span className="form-error-span">
-                {errors.singleOrigin?.message}
-              </span>
+              <SelectField
+                option={'process'}
+                options={processes}
+                register={register}
+              />
+              <span className="form-error-span">{errors.process?.message}</span>
             </div>
-          </fieldset>
-        </div>
-        <div className="form-input-label-container">
-          <Label className="edit-coffee-label" htmlFor="roastLevels">
-            Roast level
-          </Label>
-          <SelectField
-            option={'roastLevels'}
-            options={roastLevels}
-            register={register}
-          />
-          <span className="form-error-span">{errors.roastLevel?.message}</span>
-        </div>
-
-        <div className="form-input-label-container">
-          <Label className="edit-coffee-label" htmlFor="process">
-            Process
-          </Label>
-          <SelectField
-            option={'process'}
-            options={processes}
-            register={register}
-          />
-          <span className="form-error-span">{errors.process?.message}</span>
-        </div>
-
-        <div className="form-input-label-container">
-          <Label className="edit-coffee-label" htmlFor="country">
-            Country
-          </Label>
-          <SelectField
-            option={'country'}
-            options={origins}
-            register={register}
-          />
-        </div>
-        <div className="form-input-label-container">
-          <Label className="edit-coffee-label" htmlFor="notes">
-            Notes
-          </Label>
-          <textarea
-            className="edit-coffee-input text-area"
-            contentEditable
-            suppressContentEditableWarning
-            {...register('notes', {
-              maxLength: { value: 500, message: 'Maximum 500 chars' }
-            })}
-          />
-          <span className="form-error-span">{errors.notes?.message}</span>
-        </div>
+            <div className="form-input-label-container">
+              <Label className="add-coffee-label" htmlFor="country">
+                Country
+              </Label>
+              <SelectField
+                option={'country'}
+                options={origins}
+                register={register}
+              />
+            </div>
+          </div>
+        </section>
+        <section className="add-coffee-form-section two">
+          <div className="form-input-label-container">
+            <Label className="add-coffee-label" htmlFor="notes">
+              Notes
+            </Label>
+            <textarea
+              className="add-coffee-input text-area"
+              contentEditable
+              suppressContentEditableWarning
+              {...register('notes', {
+                maxLength: { value: 500, message: 'Maximum 500 chars' }
+              })}
+            />
+            <span className="form-error-span">{errors.notes?.message}</span>
+          </div>
+        </section>
       </div>
-
-      <button type="submit">Save</button>
+      <button type="submit" className="form-save-button">
+        Save
+      </button>
     </form>
   );
 };
